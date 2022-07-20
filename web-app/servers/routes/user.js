29b1f209ae079f71e8e66b35828bf29d85e5381e
mehdi <mehdikallel31@icloud.com>
2022-07-20 23:29:47 +0200

    deleted check role

--- web-app/servers/routes/user.js
+++ web-app/servers/routes/user.js
@@ -4,6 +4,7 @@ const authMiddleware = require('../middlewares/auth.js');
 
 authRouter.use('/signup/:role', authMiddleware);
 
+
 authRouter.post('/signup/:role', controller.signup);
 authRouter.get('/all/:role', controller.getAllUser);
 authRouter.post('/signin/:role', controller.signin);
