97b027194199400249cedd926c3366807f28f0e1
mehdi <mehdikallel31@icloud.com>
2022-07-21 12:35:58 +0200

    fixed connection to gateway

--- connecting.js
+++ connecting.js
@@ -2,49 +2,57 @@ const path = require("path");
 const { Wallets, Gateway, X509WalletMixin } = require("fabric-network");
 const fs = require("fs");
 
-const myChannel = "mychannel";
-const myChaincodeName = "auction";
-const identity = "user1";
+const myChannel = "agridata";
+const myChaincodeName = "trackmeat";
+const identity = "admin";
 
 async function main() {
-  const ccpPath = path.resolve(
-    __dirname,
-    "fablo-network",
-    "fablo-target",
-    "fabric-config",
-    "connection-profiles",
-    "connection-profile-farmerorg.json"
-  );
-  const fileExists = fs.existsSync(ccpPath);
-  if (!fileExists) {
-    throw new Error(`no such file or directory: ${ccpPath}`);
+  try {
+    const ccpPath = path.resolve(
+      __dirname,
+      "fablo-network",
+      "fablo-target",
+      "fabric-config",
+      "connection-profiles",
+      "connection-profile-auditor.json"
+    );
+    const fileExists = fs.existsSync(ccpPath);
+    if (!fileExists) {
+      throw new Error(`no such file or directory: ${ccpPath}`);
+    }
+
+    const contents = fs.readFileSync(ccpPath, "utf8");
+    const ccp = JSON.parse(contents);
+
+    const walletPath = path.join(
+      __dirname,
+      "fablo-network/scripts/wallet/auditor"
+    );
+    const wallet = await buildWallet(Wallets, walletPath);
+
+    const gateway = new Gateway();
+    // connect using Discovery enabled
+
+    await gateway.connect(ccp, {
+      wallet: wallet,
+      identity: identity,
+      discovery: { enabled: true, asLocalhost: true },
+    });
+
+    const network = await gateway.getNetwork(myChannel);
+    const contract = network.getContract(myChaincodeName);
+
+    const statefulTxn = await contract.createTransaction("nothing");
+    await statefulTxn.submit(
+      "Mehdi",
+      "user1",
+      "mehdikallel31",
+      "Farmer",
+      "okpok"
+    );
+  } catch (error) {
+    console.error(`******** FAILED to submit bid: ${error}`);
   }
-
-  const contents = fs.readFileSync(ccpPath, "utf8");
-  const ccp = JSON.parse(contents);
-
-  const walletPath = path.join(
-    __dirname,
-    "fablo-network/scripts/wallet/farmer"
-  );
-  const wallet = await buildWallet(Wallets, walletPath);
-
-  const gateway = new Gateway();
-  // connect using Discovery enabled
-
-  await gateway.connect(ccp, {
-    wallet: wallet,
-    identity: identity,
-    discovery: { enabled: false, asLocalhost: true },
-  });
-
-  const network = await gateway.getNetwork(myChannel);
-  const contract = network.getContract(myChaincodeName);
-
-  const statefulTxn = await contract.createTransaction("CreateAuction");
-  await statefulTxn.submit("auctionID, item, parseInt(quantity), auditor");
-
-  console.log(statefulTxn);
 }
 
 async function buildWallet(Wallets, walletPath) {
